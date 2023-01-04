eb12aac1c05beed35caffafbadc9cbd3dea83679
HugoVeraGarcia <hugo.vera.garcia@gmail.com>
2023-01-04 02:44:33 -0500

    endpoints updated

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,250 +1,225 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addNewUserThunk, loginThunk } from '../redux/actions';
-import '../styles/loginmodal.css';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addNewUserThunk, loginThunk } from "../redux/actions";
+import "../styles/loginmodal.css";
 
 const LoginModal = ({ isLoginOpen, setIsLoginOpen }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
-    const [ email, setEmail] = useState('');
-    const [ password, setPassword] = useState('');
-    const [errorMsg, setErrorMsg] = useState('');
-    
-    const [isSign, setIsSign] = useState(false)
-    
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [newEmail, setNewEmail] = useState('');
-    const [newpassword, setNewPassword] = useState('');
-    const [phone, setPhone] = useState('');
+  const [isSign, setIsSign] = useState(false);
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newpassword, setNewPassword] = useState("");
+  const [phone, setPhone] = useState("");
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const useTest = () => {
-        setEmail('hugo3@gmail.com')
-        setPassword('hugohugo')
-    }
-    const login = e => {
-        e.preventDefault();
-        if(!isSign){
-        const credentials = {
-            email,
-            password
-        }
-        
-        dispatch(loginThunk(credentials))
-        .then(res => {
-            localStorage.setItem("token", res.data.data.token);
-            setErrorMsg('');
-            setIsLoginOpen(false);
+  const useTest = () => {
+    setEmail("hugo@gmail.com");
+    setPassword("123456");
+  };
+  const login = (e) => {
+    e.preventDefault();
+    if (!isSign) {
+      const credentials = {
+        email,
+        password,
+      };
+
+      dispatch(loginThunk(credentials))
+        .then((res) => {
+          localStorage.setItem("token", res.data.data.token);
+          setErrorMsg("");
+          setIsLoginOpen(false);
         })
-        .catch(error => {
-            setErrorMsg(error.response.data.message)
+        .catch((error) => {
+          setErrorMsg(error.response.data.message);
         });
-        }else{
-            const user = {
-                firstName: firstName,
-                lastName: lastName,
-                email: newEmail,
-                password: newpassword,
-                phone: phone,
-                role: "admin"
-            }
-            
-            dispatch(addNewUserThunk(user))
-            .then(res => {
-                //localStorage.setItem("token", res.data.data.token);
-                setErrorMsg('');
-                setIsSign(false);
-            })
-            .catch(error => {
-                setErrorMsg(error.response.data.message)
-                console.log('error', error.response.data.message)
-            });
-    
-        }
-    }
+    } else {
+      const user = {
+        firstName: firstName,
+        lastName: lastName,
+        email: newEmail,
+        password: newpassword,
+        phone: phone,
+        role: "admin",
+      };
 
-    const logout = () => {
-        localStorage.removeItem('token');
-        setIsLoginOpen(false);
+      dispatch(addNewUserThunk(user))
+        .then((res) => {
+          //localStorage.setItem("token", res.data.data.token);
+          setErrorMsg("");
+          setIsSign(false);
+        })
+        .catch((error) => {
+          setErrorMsg(error.response.data.message);
+          console.log("error", error.response.data.message);
+        });
     }
+  };
 
-    const close = ()=>{
-        setIsLoginOpen(false);
-        setIsSign(false);
-    }
+  const logout = () => {
+    localStorage.removeItem("token");
+    setIsLoginOpen(false);
+  };
 
-    return (
+  const close = () => {
+    setIsLoginOpen(false);
+    setIsSign(false);
+  };
+
+  return (
+    <div>
+      {
         <div>
+          <form
+            onSubmit={login}
+            className={`login ${isLoginOpen ? "open" : ""}`}
+          >
+            {localStorage.getItem("token") ? (
+              <div className="logout_container">
+                <i
+                  className="fa-solid fa-arrow-right-from-bracket icon"
+                  onClick={logout}
+                ></i>
+                <i
+                  className="fa-solid fa-xmark icon m4rem"
+                  onClick={() => setIsLoginOpen(false)}
+                ></i>
+              </div>
+            ) : (
+              <>
+                <div className="modal">
+                  <i className="fa-solid fa-face-grin icon size"></i>
 
-            {   <div>
-                    <form onSubmit={login} className={`login ${isLoginOpen ? 'open' : '' }`} >
+                  {!isSign && (
+                    <div className="test">
+                      <p>
+                        {" "}
+                        <strong>Test</strong>
+                      </p>
+                      <div className="test_data">
+                        <i className="fa-solid fa-envelope icon"></i>
+                        <p>hugo@gmail.com</p>
+                        <i className="fa-solid fa-key icon"></i>
+                        <p>123456</p>
+                      </div>
+                      <button
+                        onClick={useTest}
+                        type="button"
+                        className="buttonTest"
+                      >
+                        Use this
+                      </button>
+                    </div>
+                  )}
 
-                        {
-                            localStorage.getItem('token') ? 
-                            
-                            
-                            <div className="logout_container">
-                                <i 
-                                    className="fa-solid fa-arrow-right-from-bracket icon"
-                                    onClick={logout}
-                                ></i>
-                            <i 
-                                className="fa-solid fa-xmark icon m4rem"
-                                onClick={()=>setIsLoginOpen(false)}
-                                >
-                            </i>
+                  {!isSign && (
+                    <input
+                      type="email"
+                      placeholder="email"
+                      className="input"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  )}
+                  {!isSign && (
+                    <input
+                      type="password"
+                      placeholder="password"
+                      className="input"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  )}
+                  {!isSign && <button className="button">Submit</button>}
+                  {!isSign && (
+                    <button
+                      onClick={() => setIsLoginOpen(false)}
+                      className="button"
+                      type="button"
+                    >
+                      Cancel
+                    </button>
+                  )}
+                  {!isSign && (
+                    <p onClick={() => setIsSign(true)} className="link">
+                      Don't have an account? Sign up
+                    </p>
+                  )}
+                  {!isSign && <p className="error">{errorMsg}</p>}
 
-                            </div>
-                            :
-                            
-                            <>
-
-
-
-                                <div className="modal"> 
-
-                                    <i className="fa-solid fa-face-grin icon size" ></i>
-                                    
-
-                                    { !isSign &&
-                                        
-                                    
-
-                                    <div className="test">
-                                        <p> <strong>Test</strong></p>
-                                        <div className="test_data">
-                                            <i className="fa-solid fa-envelope icon"></i>
-                                            <p>hugo3@gmail.com</p>
-                                            <i className="fa-solid fa-key icon"></i>
-                                            <p>hugohugo</p>
-                                        </div>
-                                        <button
-                                            onClick={useTest}
-                                            type='button'
-                                            className='buttonTest'
-                                        >Use this</button>
-                                    </div>
-                                    }
-
-                                    { !isSign &&
-
-                                    <input 
-                                    type="email" 
-                                    placeholder='email'
-                                    className='input'
-                                    value={email}
-                                    onChange={(e)=> setEmail(e.target.value)}
-                                    />
-                                    }
-                                    { !isSign &&
-                                    <input 
-                                    type="password" 
-                                    placeholder='password'
-                                    className='input'
-                                    value={password}
-                                    onChange={(e)=> setPassword(e.target.value)}
-                                    />
-                                    }
-                                    { !isSign &&
-                                    <button
-                                        className='button'
-                                    >
-                                        Submit
-                                    </button>
-                                    }
-                                    { !isSign &&
-                                    <button 
-                                        onClick={()=> setIsLoginOpen(false) }
-                                        className='button'
-                                        type='button'
-                                    >
-                                        Cancel</button>
-                                    }
-                                    { !isSign &&
-                                        
-                                        <p
-                                        onClick={()=>setIsSign(true)}
-                                        className='link'
-                                    >
-                                        Don't have an account? Sign up</p>
-
-                                    }
-                                    { !isSign &&
-                                    <p className='error'>{errorMsg}</p>
-                                    }
-
-
-                                    { isSign &&
-                                    <label htmlFor="email">Email</label>
-                                    }
-                                    { isSign &&
-                                    <input type="email" id='email' className='input' onChange={e=>setNewEmail(e.target.value)}/>
-                                    }
-                                    { isSign &&
-                                        <label htmlFor="name">First Name</label>
-                                    }
-                                    { isSign &&
-                                        <input type="text" id='name' className='input'onChange={e=>setFirstName(e.target.value)}/>
-                                    }
-                                    { isSign &&
-                                        <label htmlFor="last">Last Name</label>
-                                    }
-                                    { isSign &&
-                                        <input type="text" id='last' className='input' onChange={e=>setLastName(e.target.value)}/>
-                                    }
-                                    { isSign &&
-                                        <label htmlFor="password">Password</label>
-                                    }
-                                    { isSign &&
-                                        <input type="password" id='password' className='input'onChange={e=>setNewPassword(e.target.value)}/>
-                                    }
-                                    { isSign &&
-                                        <label htmlFor="phone">Phone number(10 characters)</label>
-                                    }
-                                    { isSign &&
-                                        <input type="text" id='phone' className='input' onChange={e=>setPhone(e.target.value)}/>
-                                    }
-                                    { isSign &&
-                                        <button
-                                            className='button'
-                                        >
-                                            Sign Up</button>
-                                    }
-                                    { isSign &&
-                                        <button
-                                            className='button'
-                                            type='button'
-                                            onClick={close}
-                                        >
-                                            Cancel</button>
-                                    }
-                                    { isSign &&
-                                    <p
-                                        onClick={()=>setIsSign(false)}
-                                        className='link'
-                                    >
-                                        Have an account? Log in</p>
-                                    }
-                                    { isSign &&
-                                    <p className='error'>{errorMsg}</p>
-                                    }
-
-                                </div>
-                            </>
-                        
-
-                            
-                        }
-
-                    </form>
+                  {isSign && <label htmlFor="email">Email</label>}
+                  {isSign && (
+                    <input
+                      type="email"
+                      id="email"
+                      className="input"
+                      onChange={(e) => setNewEmail(e.target.value)}
+                    />
+                  )}
+                  {isSign && <label htmlFor="name">First Name</label>}
+                  {isSign && (
+                    <input
+                      type="text"
+                      id="name"
+                      className="input"
+                      onChange={(e) => setFirstName(e.target.value)}
+                    />
+                  )}
+                  {isSign && <label htmlFor="last">Last Name</label>}
+                  {isSign && (
+                    <input
+                      type="text"
+                      id="last"
+                      className="input"
+                      onChange={(e) => setLastName(e.target.value)}
+                    />
+                  )}
+                  {isSign && <label htmlFor="password">Password</label>}
+                  {isSign && (
+                    <input
+                      type="password"
+                      id="password"
+                      className="input"
+                      onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                  )}
+                  {isSign && (
+                    <label htmlFor="phone">Phone number(10 characters)</label>
+                  )}
+                  {isSign && (
+                    <input
+                      type="text"
+                      id="phone"
+                      className="input"
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
+                  )}
+                  {isSign && <button className="button">Sign Up</button>}
+                  {isSign && (
+                    <button className="button" type="button" onClick={close}>
+                      Cancel
+                    </button>
+                  )}
+                  {isSign && (
+                    <p onClick={() => setIsSign(false)} className="link">
+                      Have an account? Log in
+                    </p>
+                  )}
+                  {isSign && <p className="error">{errorMsg}</p>}
                 </div>
-            
-            }
-            
+              </>
+            )}
+          </form>
         </div>
-    );
+      }
+    </div>
+  );
 };
 
 export default LoginModal;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,167 +3,203 @@
 // 3. crear la función en el archivo actions
 // 4. despachar en el componente la función creada en el paso 3
 
-
-import axios from "axios"
+import axios from "axios";
 
 export const actions = {
-    setProducts: "SET_PRODUCTS",
-    setIsLoading: "SET_IS_LOADING",
-    setCategories: "SET_CATEGORIES",
-    setFilterProducts: "SET_FILTER_PRODUCTS",
-    setName: "SET_NAME",
-    setCart: "SET_CART",
-    setPurchases: "SET_PURCHASES",
-}
-
-
+  setProducts: "SET_PRODUCTS",
+  setIsLoading: "SET_IS_LOADING",
+  setCategories: "SET_CATEGORIES",
+  setFilterProducts: "SET_FILTER_PRODUCTS",
+  setName: "SET_NAME",
+  setCart: "SET_CART",
+  setPurchases: "SET_PURCHASES",
+};
 
 const getConfig = () => ({
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+  headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
 });
 
-export const setPurchases = purchases =>({
-    type: actions.setPurchases,
-    payload: purchases
-})
+export const setPurchases = (purchases) => ({
+  type: actions.setPurchases,
+  payload: purchases,
+});
 
-export const setCart = cart => ({
-    type: actions.setCart,
-    payload: cart
-})
+export const setCart = (cart) => ({
+  type: actions.setCart,
+  payload: cart,
+});
 
-export const setProducts = products => ({
-    type: actions.setProducts,
-    payload: products
-})
+export const setProducts = (products) => ({
+  type: actions.setProducts,
+  payload: products,
+});
 
 export const setFilterProducts = (filter) => {
-    //console.log('desde actions')
-    return{
+  //console.log('desde actions')
+  return {
     type: actions.setFilterProducts,
-    payload: filter
-    }
-}
+    payload: filter,
+  };
+};
 
-export const setCategories = categories => ({
-    type: actions.setCategories,
-    payload: categories
-})
+export const setCategories = (categories) => ({
+  type: actions.setCategories,
+  payload: categories,
+});
 
-export const setIsLoading = isLoading => ({
-    type: actions.setIsLoading,
-    payload: isLoading
-})
+export const setIsLoading = (isLoading) => ({
+  type: actions.setIsLoading,
+  payload: isLoading,
+});
 
 export const getProductsThunk = () => {
-    return (dispatch) => {
-        dispatch(setIsLoading(true));
-        return axios
-            .get('https://ecommerce-api-react.herokuapp.com/api/v1/products/')
-            .then((res) =>{ dispatch(setProducts(res.data.data.products))})
-            .finally(()=> dispatch(setIsLoading(false)));
-    };
-}
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .get("https://e-commerce-api.academlo.tech/api/v1/products/")
+      .then((res) => {
+        dispatch(setProducts(res.data.data.products));
+      })
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
 export const getCategoriesThunk = () => {
-    return dispatch =>{
-        dispatch(setIsLoading(true));;
-        return axios
-            .get('https://ecommerce-api-react.herokuapp.com/api/v1/products/categories')
-            .then(res => { dispatch(setCategories(res.data.data.categories))})
-            .finally(()=> dispatch(setIsLoading(false)));
-    };
-}
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .get(
+        "https://e-commerce-api.academlo.tech/api/v1/products/categories"
+      )
+      .then((res) => {
+        dispatch(setCategories(res.data.data.categories));
+      })
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
 export const filterCategoriesThunk = (id) => {
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios
-            .get(`https://ecommerce-api-react.herokuapp.com/api/v1/products/?category=${id}`)
-            .then( res => {dispatch(setProducts(res.data.data.products))})
-            .finally(()=> dispatch(setIsLoading(false)));
-    }
-}
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .get(
+        `https://e-commerce-api.academlo.tech/api/v1/products/?category=${id}`
+      )
+      .then((res) => {
+        dispatch(setProducts(res.data.data.products));
+      })
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
 export const queryProductsThunk = (name) => {
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios
-            .get(`https://ecommerce-api-react.herokuapp.com/api/v1/products?query=${name}`)
-            .then( res => {dispatch(setProducts(res.data.data.products))})
-            .finally(()=> dispatch(setIsLoading(false)));
-    }
-}
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .get(
+        `https://e-commerce-api.academlo.tech/api/v1/products?query=${name}`
+      )
+      .then((res) => {
+        dispatch(setProducts(res.data.data.products));
+      })
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
-export const loginThunk = credentials => {
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios
-            .post('https://ecommerce-api-react.herokuapp.com/api/v1/users/login', credentials)
-            .finally(()=>dispatch(setIsLoading(false)));
-    }
-}
+export const loginThunk = (credentials) => {
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .post(
+        "https://e-commerce-api.academlo.tech/api/v1/users/login",
+        credentials
+      )
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
-export const addCartThunk = product => {
-    return dispatch =>{
-        dispatch(setIsLoading(true));
-        return axios
-            .post('https://ecommerce-api-react.herokuapp.com/api/v1/cart', product, getConfig())
-            .finally(()=> dispatch(setIsLoading(false)));
-    }
-}
+export const addCartThunk = (product) => {
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .post(
+        "https://e-commerce-api.academlo.tech/api/v1/cart",
+        product,
+        getConfig()
+      )
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
-export const addNewUserThunk = user => {
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios.post(`https://ecommerce-api-react.herokuapp.com/api/v1/users`, user)
-        .finally(()=> dispatch(setIsLoading(false)));
-    }
-}
+export const addNewUserThunk = (user) => {
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .post(`https://e-commerce-api.academlo.tech/api/v1/users`, user)
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
 export const getUserCartThunk = () => {
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios.get('https://ecommerce-api-react.herokuapp.com/api/v1/cart/', getConfig())
-            .then(res => dispatch(setCart(res.data.data.cart)))
-            .catch(error =>{
-                if (error.response.status === 404){
-                    dispatch(setCart([]))
-                }
-            })
-            .finally(()=> dispatch(setIsLoading(false)));
-    }
-}
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .get(
+        "https://e-commerce-api.academlo.tech/api/v1/cart/",
+        getConfig()
+      )
+      .then((res) => dispatch(setCart(res.data.data.cart)))
+      .catch((error) => {
+        if (error.response.status === 404) {
+          dispatch(setCart([]));
+        }
+      })
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
-export const deleteProductThunk = id =>{
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios.delete(`https://ecommerce-api-react.herokuapp.com/api/v1/cart/${id}`, getConfig())
-            .then(res => dispatch(getUserCartThunk()))
-            .finally(()=> dispatch(setIsLoading(false)));
-    }
-}
+export const deleteProductThunk = (id) => {
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .delete(
+        `https://e-commerce-api.academlo.tech/api/v1/cart/${id}`,
+        getConfig()
+      )
+      .then((res) => dispatch(getUserCartThunk()))
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
 export const purchaseCartThunk = () => {
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios.post(`https://ecommerce-api-react.herokuapp.com/api/v1/purchases`,{}, getConfig())
-        .finally(()=> dispatch(setIsLoading(false)));
-    }
-}
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .post(
+        `https://e-commerce-api.academlo.tech/api/v1/purchases`,
+        {},
+        getConfig()
+      )
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
 
 export const getPurchasesThunk = () => {
-    return dispatch => {
-        dispatch(setIsLoading(true));
-        return axios
-            .get('https://ecommerce-api-react.herokuapp.com/api/v1/purchases', getConfig())
-            .then((res) =>{ dispatch(setPurchases(res.data.data.purchases))})
-            .catch(error =>{
-                if (error.response.status === 404){
-                    dispatch(setPurchases([]))
-                }
-            })
-            .finally(()=> dispatch(setIsLoading(false)));
-    };
-}
-
+  return (dispatch) => {
+    dispatch(setIsLoading(true));
+    return axios
+      .get(
+        "https://e-commerce-api.academlo.tech/api/v1/purchases",
+        getConfig()
+      )
+      .then((res) => {
+        dispatch(setPurchases(res.data.data.purchases));
+      })
+      .catch((error) => {
+        if (error.response.status === 404) {
+          dispatch(setPurchases([]));
+        }
+      })
+      .finally(() => dispatch(setIsLoading(false)));
+  };
+};
